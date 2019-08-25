3ea17d2f6e115b21d48e8399bebfa125e34fb9cc
Erick <dracobk201@gmail.com>
2019-08-25 00:21:51 -0400

    - Main Menu añadido con sus respectivas funcionalidades. - Eliminados archivos agregados por error.

---
+++ Assets/Scripts/Main Menu/SceneHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    [Header("Data Variables")]
+    [SerializeField] private StringReference SceneToChange;
+    [SerializeField] private GameEvent ShowSceneLoading;
+
+    [Header("Script Variables")]
+    private bool isChangingSceneNow;
+    private float SceneChangingProgress;
+
+    private AsyncOperation sceneProgress;
+
+    public void SwitchScene()
+    {
+        StartCoroutine(ChangingScene());
+    }
+
+    private IEnumerator ChangingScene()
+    {
+        if (SceneManager.GetActiveScene().name.Equals(SceneToChange.Value))
+        {
+            SceneManager.LoadScene(SceneToChange.Value);
+        }
+        else
+        {
+            ShowSceneLoading.Raise();
+            yield return new WaitForSeconds(0.2f);
+
+            sceneProgress = SceneManager.LoadSceneAsync(SceneToChange.Value, LoadSceneMode.Single);
+            sceneProgress.allowSceneActivation = false;
+
+            while (!sceneProgress.isDone)
+            {
+                SceneChangingProgress = sceneProgress.progress;
+                if (sceneProgress.progress >= 0.9f)
+                {
+                    ShowSceneLoading.Raise();
+
+                    SceneChangingProgress = 0;
+                    sceneProgress.allowSceneActivation = true;
+                }
+                yield return null;
+            }
+        }
+    }
+}
--- Assets/Scripts/Utils/Global.cs
+++ Assets/Scripts/Utils/Global.cs
@@ -3,33 +3,20 @@
 public static class Global
 {
     #region Tags
-    public const string DestroyerTag = "Destroyer";
-    public const string GroundTag = "Ground";
-    public const string ObjectiveTag = "Objective";
-    public const string PlayerTag = "Player";
-    public const string PlayerBulletTag = "PlayerBullet";
-    public const string EnemyTag = "Enemy";
-    public const string EnemyBulletTag = "EnemyBullet";
+
     #endregion
 
     #region Axis
-    public const string HorizontalAxis = "Horizontal";
-    public const string VerticalAxis = "Vertical";
-    public const string JumpAxis = "Jump";
-    public const string StartAxis = "Cancel";
-    public const string FireAxis = "Fire1";
+
     #endregion
 
     #region Scene Names
     public const string MainMenuScene = "Main Menu";
-    public const string FirstLevelScene = "Level";
+    public const string FirstLevelScene = "Game";
     #endregion
 
     #region Animations
-    public const string AnimationRight = "Moving";
-    public const string AnimationUp = "FacingUp";
-    public const string AnimationJump = "Jump";
-    public const string AnimationDestroy = "Destroyed";
+
     #endregion
 
 }
